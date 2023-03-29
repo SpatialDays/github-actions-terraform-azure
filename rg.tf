59c9b52417a19f3dc13ad18bda97bc4fecdc77bb
ivica3730k <ivica-matic@outlook.com>
2023-03-29 22:08:23 +0100

    Added misc rg

--- rg.tf
+++ rg.tf
@@ -1,5 +1,10 @@
 # add a rg called test in uksouth
-resource "azurerm_resource_grop" "test" {
+resource "azurerm_resource_group" "test" {
   name     = "test"
+  location = "uksouth"
+}
+
+resource "azurerm_resource_group" "misc-rg"{
+  name = "misc-rg"
   location = "uksouth"
 }
